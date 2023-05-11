0ede0d31ec8cd560f1a4c1cf8dfe660f6e4dc8d7
asifmd <ma0632787@gmail.com>
2023-05-12 01:21:57 +0400

    angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HeaderComponent } from './common/header/header.component';
+// import { BodyComponent } from './common/body/body.component';
+// import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
